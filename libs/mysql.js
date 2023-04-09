90c48545b68cd5efa830181a6cac8149680110a6
Sahir Velasco <antharvelasco@gmail.com>
2023-04-08 20:22:01 -0500

    created enviroment variables

--- libs/mysql.js
+++ libs/mysql.js
@@ -4,7 +4,7 @@ const connection = createConnection({
   host     : 'localhost',
   port     : 3306,
   user     : 'root',
-  password : 'savg1989',
+  password : '******',
   database : 'disney'
 })
 
--- libs/mysql.pool.js
+++ libs/mysql.pool.js
@@ -1,13 +1,15 @@
 const { createPool } = require('mysql2');
+const { config } = require('./../config/config')
 const {promisify} =require('util')
 
 const pool = createPool({
-  connectionLimit: 10,
-  host: 'localhost',
-  user: 'root',
-  password: 'savg1989',
-  database: 'disney',
-});
+  host     : process.env.DB_HOST,
+  port     : process.env.DB_PORT,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_NAME
+})
+
 
 pool.query('SHOW tables', function(err, results) {
   if (err) {
